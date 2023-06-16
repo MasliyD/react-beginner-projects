aebdd3001235268c76664b9475679ba43c1c26b4
MasliyD <94189811+MasliyD@users.noreply.github.com>
2023-06-16 23:45:18 +0300

    quiz done

--- src/App.js
+++ src/App.js
@@ -1,58 +1,87 @@
+import React from 'react';
 import './index.scss';
 
 const questions = [
   {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
+    title: 'React - це ... ?',
+    variants: ['бібліотека', 'фреймворк', 'додаток'],
     correct: 0,
   },
   {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    title: 'Компонент - це ... ',
+    variants: ['додаток', 'частина дадотку або сторінки', 'то, чого я не знаю'],
     correct: 1,
   },
   {
-    title: 'Что такое JSX?',
+    title: 'Що таке JSX?',
     variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
+      'Це простой HTML',
+      'Це функция',
+      'Це той же HTML, але з можливістю виконати JS-код',
     ],
     correct: 2,
   },
 ];
 
-function Result() {
+function Result({ correct }) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <h2>Ви відгадали {correct} з {questions.length}</h2>
+      <a href='/'>
+        <button>Спробувати знову</button>
+      </a>
     </div>
   );
 }
 
-function Game() {
+function Game({ step, question, onClickVariant }) {
+  const percentage = Math.round((step / questions.length) * 100);
+
+  console.log(percentage);
+
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{ question.title }</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {
+          question.variants.map( (text, index) => (
+            <li onClick={() => onClickVariant(index)} key={text}>
+              {text}
+            </li>
+          ))
+        }
       </ul>
     </>
   );
 }
 
 function App() {
+
+  const [step, setStep] = React.useState(0);
+  const [correct, setCorrect] = React.useState(0);
+  const question = questions[step];
+
+  const onClickVariant = (index) => {
+    console.log(step, index);
+    setStep(step + 1);
+
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+    }
+  }
+
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      {
+        step != questions.length ? 
+        <Game step={step} question={question} onClickVariant={onClickVariant}/> : 
+        <Result correct={correct} />
+      }
+
     </div>
   );
 }
