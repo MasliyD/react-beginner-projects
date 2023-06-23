12c01aaa9ce2cb0e2a233f0830680b2b9fa74670
MasliyD <94189811+MasliyD@users.noreply.github.com>
2023-06-24 00:25:09 +0300

    currency done

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ function App() {
   const [fromCurrency, setFromCurrency] = React.useState('UAH');
   const [toCurrency, setToCurrency] = React.useState('USD');
   const [fromPrice, setFromPrice] = React.useState(0);
-  const [toPrice, setToPrice] = React.useState(0);
-  const [rates, setRates] = React.useState({});
+  const [toPrice, setToPrice] = React.useState(1);
+
+  // const [rates, setRates] = React.useState({});
+  const ratesRef = React.useRef({});
 
   React.useEffect(() => {
     fetch('https://api.metalpriceapi.com/v1/latest?api_key=bedbc712980ecbf2e70527bd94a47e1f&base=USD&currencies=EUR,UAH,GBP,USD')
     .then(data => data.json())
     .then(json => {
-      setRates(json.rates);
-      console.log(json.rates);
+      // setRates(json.rates);
+      ratesRef.current = json.rates;
+      onChangeToPrice(1);
     })
     .catch(err => {
       console.log('Error');
@@ -23,17 +26,25 @@ function App() {
   }, []);
   
   const onChangeFromPrice = (value) => {
-    const price = value / rates[fromCurrency];
-    const result = price * rates[toCurrency];
-    setToPrice(result);
+    const price = value / ratesRef.current[fromCurrency];
+    const result = price * ratesRef.current[toCurrency];
+    setToPrice(result.toFixed(3));
     setFromPrice(value);
   }
 
   const onChangeToPrice = (value) => {
-    const result = (rates[fromCurrency] / rates[toCurrency]) * value;
-    setFromPrice(result);
+    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
+    setFromPrice(result.toFixed(3));
     setToPrice(value);
   }
+
+  React.useEffect( () => {
+    onChangeFromPrice(fromPrice)
+  }, [fromCurrency]);
+
+  React.useEffect( () => {
+    onChangeToPrice(toPrice)
+  }, [toCurrency]);
 
   return (
     <div className="App">
